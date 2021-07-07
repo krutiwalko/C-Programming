c4f47464688719df03a9141e8b84a77e6da0978a
krutiwalko <69305743+krutiwalko@users.noreply.github.com>
2021-07-07 23:02:16 +0530

    Update swap_number.c

--- swap_number.c
+++ swap_number.c
@@ -4,11 +4,11 @@ int main()
 {
 	int a,b;
 	printf("Enter two numbers separated by space: ");
-	scanf("%d %d",&a,&b);
+	scanf("%d %d",&a,&b); //if a=1 and b=2
 	printf("Before swapping: a=%d and b=%d\n",a,b);
-	a=a+b;
-	b=a-b;
-	a=a-b;
+	a=a+b; //a=3
+	b=a-b; //b=3-2=1
+	a=a-b; //a=3-1=2
 	printf("After swapping: a=%d and b=%d",a,b);
 	return 0;
 }
